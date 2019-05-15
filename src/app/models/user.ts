277d684849f04a1b74c95097943f72ccec87cef5
Guilherme França <guilhermedepaula.ti@gmail.com>
2019-05-14 21:12:42 -0300

    Create a model to user

---
+++ src/app/models/user.ts
@@ -0,0 +1,14 @@
+export class User {
+  username: string;
+  name: string;
+  email: string;
+  city: string;
+  often_ride: string;
+}
+
+
+
+export interface ITable {
+  field: string;
+  header: string;
+}
